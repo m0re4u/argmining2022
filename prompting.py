969dee2c1149a31e1b6a36ecb3505c35ab73baca
Selene Baez <s.baezsantamaria@vu.nl>
2022-07-29 10:26:06 +0200

    two gpt3 runs: prompt style 2 and 3

--- prompting.py
+++ prompting.py
@@ -1,16 +1,15 @@
 import argparse
 import json
 import os
-from typing import List
 import time
 
+import numpy as np
 import openai
 from dotenv import load_dotenv
 from tqdm import tqdm
-import numpy as np
 
-from data import SharedTaskConstants, SharedTaskData
 from baseline import print_results
+from data import SharedTaskConstants, SharedTaskData
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -22,7 +21,7 @@ def gpt3(prompts, model="text-davinci-002"):
         model=model,
         prompt=prompts,
         temperature=0,
-        max_tokens=256,
+        max_tokens=50,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
@@ -58,28 +57,30 @@ def get_examples(n: int, example_data, prompt_style: int, goal: str) -> str:
             prompt_style=prompt_style,
             goal=goal,
             label=label))
-    return "\n".join(example_prompts)
+    return "\n\n".join(example_prompts)
 
 
 def get_prompt(sample, prompt_style: int, goal: str):
     """
     Helper function for getting a prompt given a dataset instance.
     """
-    return get_prompt_text(sample['topic'], sample['premise'], sample['conclusion'], prompt_style=prompt_style, goal=goal)
+    return get_prompt_text(sample['topic'], sample['premise'], sample['conclusion'], prompt_style=prompt_style,
+                           goal=goal)
 
 
-def get_prompt_text(
-        topic: str,
-        premise: str,
-        conclusion: str,
-        prompt_style: int = 0,
-        goal: str = 'novelty',
-        label: str = None
-        ) -> str:
+def get_prompt_text(topic: str,
+                    premise: str,
+                    conclusion: str,
+                    prompt_style: int = 0,
+                    goal: str = 'novelty',
+                    label: str = None) -> str:
+    """ Function to create a prompt """
+
     if goal != "novelty" and goal != "validity":
         raise ValueError(f"Unknown prompt task {goal}")
 
     task_prompt = {}
+    # Baseline
     if prompt_style == 0:
         task_prompt['novelty'] = "Is there a presence of novel premise-related content? "
         task_prompt['validity'] = "Is there a strict logical inference that links the premise to the conclusion? "
@@ -88,6 +89,8 @@ premise {premise}
 conclusion {conclusion}
 {task_prompt[goal]}
 """
+
+    # Capitalized and colon
     elif prompt_style == 1:
         task_prompt['novelty'] = "Is there a presence of novel premise-related content? "
         task_prompt['validity'] = "Is there a strict logical inference that links the premise to the conclusion? "
@@ -96,13 +99,32 @@ PREMISE: {premise}
 CONCLUSION: {conclusion}
 {task_prompt[goal]}
 """
+
+    # Dynamic prompts
     elif prompt_style == 2:
-        task_prompt['novelty'] = "NOVELTY: "
-        task_prompt['validity'] = "VALIDITY: "
+        task_prompt['novelty'] = "NOVELTY:"
+        task_prompt['validity'] = "VALIDITY:"
         prompt = f"""TOPIC: {topic}
 PREMISE: {premise}
 CONCLUSION: {conclusion}
 {task_prompt[goal]}"""
+
+    # Static merged prompts
+    elif prompt_style == 3:
+        prompt = f"""TOPIC: Torture
+PREMISE: A terrorist will experience pain for a short period while being tortured. Yet, the millions of lives that could be lost if that pain is not inflicted will be gone forever. The ethical trade-off is overwhelmingly in favor of performing torture.
+CONCLUSION: Pain of torture is worth suffering
+VALIDITY:not-valid, NOVELTY:novel
+
+TOPIC: United Nations Standing Army
+PREMISE:especially if it were to include purchase of air and sea transport to reach theatres of operation, added to the high costs of permanent establishment and training, and equipping the force for every possible type of terrain. At present the UN can draw upon different kind of troops for different kinds of missions from whatever member states feel best equipped to deal with a particular situation.
+CONCLUSION: The cost of such an army would be very high,
+VALIDITY:valid, NOVELTY:not-novel
+
+TOPIC: {topic}
+PREMISE: {premise}
+CONCLUSION: {conclusion}"""
+
     else:
         raise ValueError(f"Unknown prompt style: {prompt_style}")
 
@@ -127,7 +149,8 @@ def parse_response(api_response, label: str, prompt_style: int):
             return label
         else:
             return f"not-{label}"
-    elif prompt_style == 2:
+
+    elif prompt_style in [2, 3]:
         if f"not-{label}" in api_response['text']:
             return f"not-{label}"
         else:
@@ -153,22 +176,24 @@ def main(args):
         # Get the prompt for asking about the sample
         x_val = get_prompt(sample, prompt_style=args.prompt_style, goal='validity')
         x_nov = get_prompt(sample, prompt_style=args.prompt_style, goal='novelty')
-        if args.n_shot > 0:
+        if args.n_shot > 0 and args.prompt_style != 3:
             # Get examples
             example_prompt_val = get_examples(args.n_shot, train_data, prompt_style=args.prompt_style, goal='validity')
             example_prompt_nov = get_examples(args.n_shot, train_data, prompt_style=args.prompt_style, goal='novelty')
             # Prepend examples
-            x_val = f"{example_prompt_val}\n{x_val}"
-            x_nov = f"{example_prompt_nov}\n{x_nov}"
-        # Pass through model
-        response = gpt3([x_val, x_nov])
+            x_val = f"{example_prompt_val}\n\n{x_val}"
+            x_nov = f"{example_prompt_nov}\n\n{x_nov}"
 
-        # Ugly hack to adhere to rate limit (1 / s)
-        if args.prompt_style == 2:
-            time.sleep(1)
+        if args.prompt_style != 3:
+            # Pass through model
+            response = gpt3([x_val, x_nov])
+            assert len(response) == 2
+            response_val, response_nov = response
 
-        assert len(response) == 2
-        response_val, response_nov = response
+        else:
+            # Pass through model
+            response = gpt3([x_val])
+            [response_val], [response_nov] = response, response
 
         # Parse responses
         pred_val = parse_response(response_val, 'valid', prompt_style=args.prompt_style)
@@ -187,8 +212,12 @@ def main(args):
             'pred_val': pred_val,
         })
 
+        # Ugly hack to adhere to rate limit (1 / s)
+        if args.prompt_style in [2, 3]:
+            time.sleep(1)
+
     # Write results to file as backup
-    with open(f'dump_prompt_{args.prompt_style}_results.json', 'w') as f:
+    with open(f'predictions/dump_prompt_{args.prompt_style}_results.json', 'w') as f:
         json.dump(dump_data, f)
 
     print_results(f"Prompting (style {args.prompt_style})", y_true, y_pred)
@@ -196,8 +225,9 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--prompt_style', default=0, type=int, choices=[0, 1, 2],
-                        help="Which prompt style to use")
+    parser.add_argument('--prompt_style', default=0, type=int, choices=[0, 1, 2, 3],
+                        help="Which prompt style to use: "
+                             "0 baseline, 1 capitalized, 2 random dynamic examles, 3 static examples, merged prompts")
     parser.add_argument('--n_shot', default=0, type=int,
                         help="How many examples to give in the prompt")
     args = parser.parse_args()
